194ebf3f3ff08431e53e64216437e23c7739d2bc
Ninja <sleepyneko@protonmail.ch>
2025-01-25 16:53:35 +0200

    Bubels pop + dirt alpha

--- BubbleTrouble/Assets/Scripts/BubbleShot.cs
+++ BubbleTrouble/Assets/Scripts/BubbleShot.cs
@@ -1,27 +1,53 @@
+using System.Collections;
 using UnityEngine;
 
 public class BubbleShot : MonoBehaviour
 {
     public int CleanRadius = 10;
     public int Damage = 10;
-    
+    private Material bubbleMat;
+
+    private void Start()
+    {
+        bubbleMat = GetComponent<MeshRenderer>().material;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
             collision.gameObject.GetComponentInParent<Enemy>().TakeDamage(Damage);
-            Destroy(gameObject);
+            StartCoroutine(FadeOut());
         }
 
         if (collision.gameObject.CompareTag("Wall"))
         {
-            Destroy(gameObject);
+            StartCoroutine(FadeOut());
         }
 
-        if (collision.gameObject.CompareTag("Floor") && collision.gameObject.GetComponent<CleaningTest>() is CleaningTest cleaning)
+        if (collision.gameObject.CompareTag("Floor"))
         {
-            cleaning?.Clean(transform.position, collision.GetContact(0).point, CleanRadius);
-            Destroy(gameObject);
+            if (collision.gameObject.GetComponent<CleaningTest>() is CleaningTest cleaning)
+            {
+                cleaning?.Clean(transform.position, collision.GetContact(0).point, CleanRadius);
+            }
+
+            StartCoroutine(FadeOut());
         }
+    }
+
+    IEnumerator FadeOut()
+    {
+        GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+        GetComponent<Collider>().enabled = false;
+        float fade = 0;
+        while (fade < 1)
+        {
+            fade += Time.deltaTime * 5;
+            bubbleMat.SetFloat("_Dissolve", fade);
+            yield return null;
+        }
+
+        Destroy(gameObject);
     }
 }
