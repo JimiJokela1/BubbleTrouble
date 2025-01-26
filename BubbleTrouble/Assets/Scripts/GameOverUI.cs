e5a8147b69beea1dcff4458924af52955f5ef151
Jimi Jokela <jimi.jokela@metaplay.io>
2025-01-26 15:54:18 +0200

    Add door opening

--- BubbleTrouble/Assets/Scripts/GameOverUI.cs
+++ BubbleTrouble/Assets/Scripts/GameOverUI.cs
@@ -29,6 +29,11 @@ public class GameOverUI : MonoBehaviour
                 Restart();
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 
     public void ShowGameOver()
